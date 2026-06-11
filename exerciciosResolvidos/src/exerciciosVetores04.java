090037382750652af3b560aa2dfd6bbec329da6f
Osmar-Henrique <henrique.valtrig@gmail.com>
2026-06-11 20:59:27 -0300

    Update exerciciosVetores04.java

--- exerciciosResolvidos/src/exerciciosVetores04.java
+++ exerciciosResolvidos/src/exerciciosVetores04.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class exerciciosVetores04 {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String[] cidades = new String[5];
 
 
         }
