297209b9ccfeffa30463c9ec93518b12cd36f066
Osmar-Henrique <henrique.valtrig@gmail.com>
2026-06-18 20:43:27 -0300

    Create exercicioVetores08.java
    Atualiza exercícios

---
+++ exerciciosResolvidos/src/exercicioVetores08.java
@@ -0,0 +1,24 @@
+public class exercicioVetores08 {
+    public static void main(String[] args) {
+
+        int[] v = {4, 17, 3, 22, 8, 15, 1, 30, 9};
+        int maiorN=0, menorN =0;
+
+
+        for (int i = 1; i < v.length; i++)
+        {
+            if (v[i] > v[maiorN])
+            {
+                maiorN = i;
+            }
+            else if (v[i] < v[menorN])
+            {
+                menorN = i;
+            }
+        }
+        System.out.println(v[maiorN]);
+        System.out.println("Menor: v[" + menorN + "] = " + v[menorN]);
+
+
+    }
+}
